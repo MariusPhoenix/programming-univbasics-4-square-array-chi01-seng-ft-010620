a927f22eca1eef00cfcaa58a15a3ab39138b758a
Marius Phoenix <phoenixzenomarius@gmail.com>
2019-12-23 06:56:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,7 @@
-
- def square_array
-   counter = 0 
-  array = [1,2,3]
-  
-  while array(counter)
-  square_array(array).inject
-  counter += 1
+def square_array(array)
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
-
-end
-  
+ 
+ 
